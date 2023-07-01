64891865f1533bfb26c9da9fb9f2cfde8eb29931
ngthgph <ngthgph112@gmail.com>
2023-07-01 18:21:11 +0700

    specify the type of a function parameter or a return type

--- Main.kt
+++ Main.kt
@@ -2,8 +2,13 @@ fun main() {
     val trickFunction = trick
     trick()
     trickFunction()
+    treat()
 }
 
 val trick = {
     println("No treats!")
+}
+
+val treat: () -> Unit = {
+    println("Have a treat!")
 }
